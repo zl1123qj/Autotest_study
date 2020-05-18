13f887b3e4dc53edda5252c6d033a996831720bd
zl1123qj <472832640@qq.com>
2020-05-18 11:58:42 +0800

    commit files

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,3 @@
+# coding = utf-8
+
+EXCEL_DIR="F:\\云\\3333\\移动测试\\pytest-demo\\data\\search_query.xlsx"
---
+++ src/httpbin_api/const.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+BASE_URL = "http://httpbin.org/"
+IP_URL = "/ip"
+LOCAL_IP = "222.208.97.197"
+POST_TEST_URL = "/post"
